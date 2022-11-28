fc57bfc93e426f40b12f684f93efa807cad679a4
v.gordeev <v.gordeev@okko.tv>
2022-11-28 22:37:37 +0300

    java-setup-environment: dependencies

---
+++ java/io/hexlet/Example.java
@@ -0,0 +1,9 @@
+package io.hexlet;
+
+import org.apache.commons.lang3.RandomUtils;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println(RandomUtils.nextInt(1, 1000));
+    }
+}
--- java/io/hexlet/One.java
+++ java/io/hexlet/One.java
@@ -1,7 +1,9 @@
 package io.hexlet;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class One {
     public static void main(String[] args) {
-        Two.greeting();
+        System.out.println(StringUtils.reverse("Hexlet"));
     }
 }
